9c916460e61587e42ba384f0781c369f630bc30f
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-11 19:01:56 -0300

    feat: add index to initializing server'

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import consumer from './infra/kafka/consumer'
+import SendMailKafkaService from './application/services/send-mail-kafka-service'
+
+(async () => {
+  const mailService = new SendMailKafkaService()
+  await consumer.connect()
+  await consumer.subscribe([
+    {
+      topic: 'mailer-event'
+    }
+  ])
+  await consumer.run(mailService.handler.bind(mailService))
+})()
