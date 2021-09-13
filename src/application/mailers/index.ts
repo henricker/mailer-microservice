dddfb78d3f3226a11d0e1b4d8653a014f38aed87
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-13 16:27:56 -0300

    refactor: add mailers reference

--- src/application/mailers/index.ts
+++ src/application/mailers/index.ts
@@ -1,5 +1,9 @@
 import WelcomeUserMailer from "./welcome-user"
+import ForgotPasswordUserMailer from "./forgot-password"
+import NewBetUserMailer from "./new-bet-user"
 
 export default {
   'welcome-user': WelcomeUserMailer,
+  'forgot-password-user': ForgotPasswordUserMailer,
+  'new-bet-user': NewBetUserMailer
 }
