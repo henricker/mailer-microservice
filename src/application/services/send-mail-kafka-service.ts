bc5ff7f70d979491fdf0af42468bd3440d5814bd
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-13 16:25:46 -0300

    refactor: add try catch

--- src/application/services/send-mail-kafka-service.ts
+++ src/application/services/send-mail-kafka-service.ts
@@ -2,19 +2,24 @@ import { EachMessagePayload } from "kafkajs";
 import { mailerOptions } from "../../infra/config/mailer/mailer.config";
 import KafkaService from "../../infra/kafka/service-kafka";
 import mailers from "../mailers";
-import IPayloadMailer from "../mailers/IPayloadMailer";
+import IPayloadMailer from "../mailers/interfaces/IPayloadMailer";
 
 export default class SendMailKafkaService implements KafkaService  {
   constructor() {}
 
   async handler(payloadTopic: EachMessagePayload): Promise<void> {
-    const payloadString = payloadTopic.message.value?.toString()
-    if(!payloadString) return
-
-    const payload: IPayloadMailer = JSON.parse(payloadString)
-    const mailer = new mailers[payload.handleMailer](mailerOptions, payload)
-    await mailer.send()
+    try {
+      const payloadString = payloadTopic.message.value!.toString()
+      const payload: IPayloadMailer = JSON.parse(payloadString) 
+      const mailerClass = mailers[payload.template]
+      const mailer = new mailerClass(mailerOptions, payload)
+      await mailer.send()
+    } catch(err) {
+      console.log(`Error for message: ${payloadTopic.message.value?.toString()}`)
+      console.error(err)
+    }
   }
 }
 
   // testing {"contact":{"name":"henricker","email":"henricker@email.com"},"handleMailer":"welcome-user"}
+  // {"contact":{"name":"Arianna Ebert","email":"henricker@email.com"},"bets":{"totalPrice":"R$ 9,00","arrayBets":[{"game":"Mega-Sena","color":"#01AC66","numbers":"16,35,48,16,60,52"},{"game":"Mega-Sena","color":"#01AC66","numbers":"1,25,30,24,25,10"}]},"template":"new-bet-user"}
