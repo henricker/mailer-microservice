290a535a7e98930ebbe7bb143a1acca4cbe2ee6b
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-14 15:14:38 -0300

    refactor: add methods to process, and events functions

--- src/infra/bull/queue.ts
+++ src/infra/bull/queue.ts
@@ -1,11 +1,11 @@
-import Bull, { Queue } from 'bull';
+import Bull, { Job, JobPromise, Queue } from 'bull';
 import { queueOptions } from '../config/bull/bull.config';
-import JobContract, { IJobContract } from './jobContract';
+import JobContract from './jobContract';
 
 interface IQueueBull{ 
   bull: Queue<any>; 
   name: string; 
-  handle: ({data}: IJobContract<any>) => Promise<Object>
+  handle: ({data}: any) => Promise<void>
 }
 
 export default class QueuesBull {
@@ -22,17 +22,33 @@ export default class QueuesBull {
 
   public add(name: string, data: any) {
     const queue = this.queues.find(queue => queue.name === name)
-    return queue?.bull.add(data)
+    return queue?.bull.add(data, { 
+      attempts: 5, 
+      removeOnComplete: true, 
+      removeOnFail: true, 
+      delay: 500 
+    })
   }
 
-  process(): void {
-    this.queues.forEach(queue => {
-      queue.bull.process(queue.handle)
-
-      queue.bull.on('failed', (job, err) => {
-        console.log('Job failed', queue.name, job.data)
-        console.log(err)
-      })
+  async process(): Promise<void> {
+    this.queues.forEach(async queue => {
+      queue!.bull.on('active', (job, jobPromise) => this.onStart(job, queue, jobPromise))
+      queue!.bull.on('failed', (job, err) => this.onFailed(job, err, queue))
+      queue!.bull.on('completed', (job, result) => this.onComplete(job, result, queue))
+      await queue.bull.process(queue.handle)
     })
+  }
+
+  private onFailed(job: Job, err: Error, queue: IQueueBull): void {
+    console.log(`${queue.name} Job failed`)
+    console.error(err.message)
+  }
+
+  private onComplete(job: Job, result: any, queue: IQueueBull): void {
+    console.log(`${queue.name} Job finish with success!`)
+  }
+
+  private onStart(job: Job, queue: IQueueBull, jobPromise?: JobPromise,) {
+    console.log(`${queue.name} Job starting...`, )
   }
 }
