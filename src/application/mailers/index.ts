ab4be20ada99b0fda4535f49933d66ff27daae50
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-14 08:37:00 -0300

    refactor: add RememberUserToBetMailer on index exp

--- src/application/mailers/index.ts
+++ src/application/mailers/index.ts
@@ -1,9 +1,11 @@
 import WelcomeUserMailer from "./welcome-user"
-import ForgotPasswordUserMailer from "./forgot-password"
+import ForgotPasswordUserMailer from "./forgot-password-user"
 import NewBetUserMailer from "./new-bet-user"
+import RememberUserToBetMailer from "./remember-user-to-bet"
 
 export default {
   'welcome-user': WelcomeUserMailer,
   'forgot-password-user': ForgotPasswordUserMailer,
-  'new-bet-user': NewBetUserMailer
+  'new-bet-user': NewBetUserMailer,
+  'remember-user-to-bet': RememberUserToBetMailer
 }
