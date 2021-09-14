1555f8dc7176e79e98101522ecb792a0802eee92
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-14 19:45:10 -0300

    refactor: initialize kafkaconsume and inject on Consumer

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,8 +1,11 @@
-import consumer from '../infra/kafka/consumer'
+import { consumerConfig } from '../infra/config/kafka/kafka.consumer.config'
+import Consumer from '../infra/kafka/consumer'
+import kafka from '../infra/kafka/kafka'
 import SendMailKafkaService from './services/send-mail-kafka-service'
 
 (async () => {
   const mailService = new SendMailKafkaService()
+  const consumer = new Consumer(kafka.consumer(consumerConfig))
   await consumer.connect()
   await consumer.subscribe([
     {
