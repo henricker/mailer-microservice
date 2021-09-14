3d379f2d2c352786be11473f436fa8c0eeac48d4
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-14 15:13:36 -0300

    refactor: remove constructor and create job

--- src/application/services/send-mail-kafka-service.ts
+++ src/application/services/send-mail-kafka-service.ts
@@ -1,19 +1,17 @@
 import { EachMessagePayload } from "kafkajs";
 import { mailerOptions } from "../../infra/config/mailer/mailer.config";
 import KafkaService from "../../infra/kafka/service-kafka";
-import mailers from "../mailers";
+import MailerJob from "../jobs/mailer-job";
+import queues from "../jobs/queue";
 import IPayloadMailer from "../mailers/interfaces/IPayloadMailer";
 
 export default class SendMailKafkaService implements KafkaService  {
-  constructor() {}
-
   async handler(payloadTopic: EachMessagePayload): Promise<void> {
     try {
       const payloadString = payloadTopic.message.value!.toString()
-      const payload: IPayloadMailer = JSON.parse(payloadString) 
-      const mailerClass = mailers[payload.template]
-      const mailer = new mailerClass(mailerOptions, payload)
-      await mailer.send()
+      const payload: IPayloadMailer = JSON.parse(payloadString)
+      const mailerJob = new MailerJob()
+      queues.add(mailerJob.key, { payload, mailerOptions })
     } catch(err) {
       console.log(`Error for message: ${payloadTopic.message.value?.toString()}`)
       console.error(err)
