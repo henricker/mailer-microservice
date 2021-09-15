e97d96af79b6ee8c18e18f6edc7bda52771c352b
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-15 16:17:26 -0300

    refactor: add games object on payload

--- src/application/mailers/interfaces/IPayloadMailer.ts
+++ src/application/mailers/interfaces/IPayloadMailer.ts
@@ -1,12 +1,18 @@
-import mailers from "..";
-import IBet from "../../models/bet";
+import mailers from "../index";
 import IContact from "../../models/contact";
+import IGame from "../../models/game";
+import IBet from "../../models/bet";
 
 export default interface IPayloadMailer {
   contact: IContact,
-  bets?: {
+  games?: {
     totalPrice: number,
-    arrayBets: IBet[]
+    gamesRelatory: IGame[]
   },
+  bets?: {
+    totalPrice: number
+    arrayBets: IBet[]
+  }
   template: keyof typeof mailers
+  kind: 'IPayloadMailerAdmin'
 }
