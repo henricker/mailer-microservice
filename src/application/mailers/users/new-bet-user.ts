02d4297ad7399ae6bbae1f5799893d18987e9fb3
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-15 16:18:05 -0300

    feat: add new-bets-user mailer

---
+++ src/application/mailers/users/new-bet-user.ts
@@ -0,0 +1,41 @@
+import { readFile } from "fs/promises";
+import { Transporter } from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import { resolve } from "path";
+import BaseMailer from "../../../infra/mailer/basemailer";
+import handlebars from 'handlebars';
+import IPayloadMailer from "../interfaces/IPayloadMailer";
+
+export default class NewBetUserMailer extends BaseMailer {
+  constructor(authConfig: SMTPTransport.Options, private payload: IPayloadMailer) {    
+    super(authConfig)
+  }
+
+  async prepare(transporter: Transporter): Promise<void> {
+
+    const templateFileContent = (await readFile(resolve(
+      __dirname, 
+      '..',
+      '..',  
+      'resources', 
+      'views', 
+      'mails', 
+      'new-bet-user.hbs',
+    ))).toString('utf-8')
+
+    const mailTemplateParse = handlebars.compile(templateFileContent)
+
+    const html = mailTemplateParse({
+      name: this.payload.contact.name,
+      bets: this.payload.bets!.arrayBets,
+      totalPrice: this.payload.bets?.totalPrice
+    })
+
+    await transporter.sendMail({
+      from: 'TGL BETS 🍀 <tgl@suport.com>',
+      to: this.payload.contact.email,
+      subject: 'Bets placed!',
+      html
+    })
+  }
+}
