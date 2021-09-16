c9665e52658eee07791d659c2947cbfb8650c96c
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-15 22:01:41 -0300

    style: remove tabs

--- src/application/mailers/index.ts
+++ src/application/mailers/index.ts
@@ -5,9 +5,9 @@ import RememberUserToBetMailer from "./users/remember-user-to-bet"
 import NewBetAdminMailer from "./admins/new-bet-admin"
 
 export default {
-    'welcome-user': WelcomeUserMailer,
-    'forgot-password-user': ForgotPasswordUserMailer,
-    'new-bet-user': NewBetUserMailer,
-    'remember-user-to-bet': RememberUserToBetMailer,
-    'new-bets-admin': NewBetAdminMailer
+	'welcome-user': WelcomeUserMailer,
+	'forgot-password-user': ForgotPasswordUserMailer,
+	'new-bet-user': NewBetUserMailer,
+	'remember-user-to-bet': RememberUserToBetMailer,
+	'new-bets-admin': NewBetAdminMailer
 }
