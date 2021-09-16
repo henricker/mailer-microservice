57e2986d5fa0d16e2e63b98b5f25edc82dfb72cb
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-15 23:04:51 -0300

    refactor: change import to new name dir of kafkaconsumers

--- src/application/index.ts
+++ src/application/index.ts
@@ -1,7 +1,7 @@
 import { consumerConfig } from '../infra/config/kafka/kafka.consumer.config'
 import Consumer from '../infra/kafka/consumer'
 import kafka from '../infra/kafka/kafka'
-import SendMailKafkaService from './services/send-mail-kafka-service'
+import SendMailKafkaService from './kafkaconsumers/send-mail-kafka-service'
 
 (async () => {
   const mailService = new SendMailKafkaService()
