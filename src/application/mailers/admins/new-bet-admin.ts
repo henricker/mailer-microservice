6ce2c5a5727265366aff91e1a2c02681fef7d941
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-16 20:46:46 -0300

    refactor: refactor imports of HandlebarsCompilerService

--- src/application/mailers/admins/new-bet-admin.ts
+++ src/application/mailers/admins/new-bet-admin.ts
@@ -2,7 +2,7 @@ import { Transporter } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import BaseMailer from "../../../infra/mailer/basemailer";
 import IPayloadMailerAdmin from "../interfaces/IPayloadMailer";
-import HandlebarsCompilerService from "../../handlebars/handlebars";
+import HandlebarsCompilerService from "../../../infra/handlebars/handlebars";
 
 export default class NewBetAdminMailer extends BaseMailer {
   constructor(authConfig: SMTPTransport.Options, private payload: IPayloadMailerAdmin) {    
--- src/application/mailers/users/new-bet-user.ts
+++ src/application/mailers/users/new-bet-user.ts
@@ -2,7 +2,7 @@ import { Transporter } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
 import BaseMailer from "../../../infra/mailer/basemailer";
 import IPayloadMailer from "../interfaces/IPayloadMailer";
-import HandlebarsCompilerService from "../../handlebars/handlebars";
+import HandlebarsCompilerService from "../../../infra/handlebars/handlebars";
 
 export default class NewBetUserMailer extends BaseMailer {
   constructor(authConfig: SMTPTransport.Options, private payload: IPayloadMailer) {    
