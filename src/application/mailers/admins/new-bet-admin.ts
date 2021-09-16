b7e06be86a762f7e7eaba335b29730a5ed0531a9
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-16 15:36:16 -0300

    refactor: add instance to simplify code

--- src/application/mailers/admins/new-bet-admin.ts
+++ src/application/mailers/admins/new-bet-admin.ts
@@ -1,10 +1,8 @@
-import { readFile } from "fs/promises";
 import { Transporter } from "nodemailer";
 import SMTPTransport from "nodemailer/lib/smtp-transport";
-import { resolve } from "path";
 import BaseMailer from "../../../infra/mailer/basemailer";
-import handlebars from 'handlebars';
 import IPayloadMailerAdmin from "../interfaces/IPayloadMailer";
+import HandlebarsCompilerService from "../../handlebars/handlebars";
 
 export default class NewBetAdminMailer extends BaseMailer {
   constructor(authConfig: SMTPTransport.Options, private payload: IPayloadMailerAdmin) {    
@@ -13,19 +11,8 @@ export default class NewBetAdminMailer extends BaseMailer {
 
   async prepare(transporter: Transporter): Promise<void> {
 
-    const templateFileContent = (await readFile(resolve(
-      __dirname, 
-      '..',
-      '..',  
-      'resources', 
-      'views', 
-      'mails', 
-      'new-bets-admin.hbs',
-    ))).toString('utf-8')
-
-    const mailTemplateParse = handlebars.compile(templateFileContent)
-
-    const html = mailTemplateParse({
+    const hbs = new HandlebarsCompilerService('new-bets-admin')
+    const html = await hbs.compile({
       name: this.payload.contact.name,
       games: this.payload.games!.gamesRelatory,
       totalPrice: this.payload.games!.totalPrice
