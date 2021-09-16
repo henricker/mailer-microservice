7f74d7a68e8434882192652b37e4a2eea8f9c12e
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-15 22:01:19 -0300

    refactor: remove unused hbs on transporter compile

--- src/infra/mailer/basemailer.ts
+++ src/infra/mailer/basemailer.ts
@@ -1,19 +1,11 @@
 import { createTransport, Transporter } from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
-import hbs from 'nodemailer-express-handlebars'
-import { resolve } from 'path';
 
 export default abstract class BaseMailer {
   private transporter: Transporter<SMTPTransport.SentMessageInfo>
 
   constructor(private authConfig: SMTPTransport.Options) {
     this.transporter = createTransport(authConfig)
-    this.transporter.use('compile', hbs({
-      viewEngine: {
-        extname: '.hbs',
-      },
-      viewPath: resolve(__dirname, '..', 'application', 'resources', 'views', 'mails')
-    }))
   }
 
   protected abstract prepare(transporter: Transporter): Promise<void>
