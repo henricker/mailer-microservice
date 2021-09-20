ab1a1d33680978049bffe4e357b44f68969834bb
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-20 13:13:51 -0300

    feat: add reports mailers

--- src/application/mailers/index.ts
+++ src/application/mailers/index.ts
@@ -2,12 +2,16 @@ import WelcomeUserMailer from "./users/welcome-user"
 import ForgotPasswordUserMailer from "./users/forgot-password-user"
 import NewBetUserMailer from "./users/new-bet-user"
 import RememberUserToBetMailer from "./users/remember-user-to-bet"
-import NewBetAdminMailer from "./admins/new-bet-admin"
+import DailyReportsAdminMailer from "./admins/daily-reports"
+import WeeklyReportsAdminMailer from "./admins/weekly-reports"
+import MonthlyReportsAdminMailer from "./admins/monthly-reports"
 
 export default {
 	'welcome-user': WelcomeUserMailer,
 	'forgot-password-user': ForgotPasswordUserMailer,
 	'new-bet-user': NewBetUserMailer,
 	'remember-user-to-bet': RememberUserToBetMailer,
-	'new-bets-admin': NewBetAdminMailer
+	'daily-reports-admin': DailyReportsAdminMailer,
+	'weekly-reports-admin': WeeklyReportsAdminMailer,
+	'monthly-reports-admin': MonthlyReportsAdminMailer,
 }
