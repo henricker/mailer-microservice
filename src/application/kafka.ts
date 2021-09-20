7abf97fa7cc4c85eb548e9e0c7ab12c5c488d620
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-20 13:13:22 -0300

    refactor: change execute to running all services

--- src/application/kafka.ts
+++ src/application/kafka.ts
@@ -1,3 +1,4 @@
+import { ConsumerSubscribeTopic } from 'kafkajs'
 import { consumerConfig } from '../infra/config/kafka/kafka.consumer.config'
 import Consumer from '../infra/kafka/consumer'
 import kafka from '../infra/kafka/kafka'
@@ -6,10 +7,14 @@ import * as kafkaServices from './kafkaconsumers/'
 (async () => {
   const consumer = new Consumer(kafka.consumer(consumerConfig))
   await consumer.connect()
-  await consumer.subscribe([
-    {
-      topic: 'mailer-event'
+  
+  const topics = Object.values(kafkaServices).map((service) => {
+    const topic: ConsumerSubscribeTopic = {
+      topic: service.topic
     }
-  ])
+    return topic
+  })
+
+  await consumer.subscribe(topics)
   Object.values(kafkaServices).forEach(async (service) => await consumer.run(service))
 })()
