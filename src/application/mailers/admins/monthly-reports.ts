63cec481fee6a2a6dc9758fda103eb7ba00488c3
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-20 13:08:01 -0300

    feat: add reports mailers

---
+++ src/application/mailers/admins/monthly-reports.ts
@@ -0,0 +1,34 @@
+import { Transporter } from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import BaseMailer from "../../../infra/mailer/base-mailer";
+import IPayloadMailer from "../interfaces/IPayloadMailer";
+import HandlebarsCompilerService from "../../../infra/handlebars/handlebars";
+
+export default class MonthlyReportsAdminMailer extends BaseMailer {
+  constructor(authConfig: SMTPTransport.Options, private payload: IPayloadMailer) {    
+    super(authConfig)
+  }
+
+  async prepare(transporter: Transporter): Promise<void> {
+
+    if(!this.payload.contact || !this.payload.contact.name || !this.payload.contact.email)
+      throw new Error('invalid contact')
+
+    if(!this.payload.games || this.payload.games.gamesRelatory.length === 0 || !this.payload.games.totalPrice)
+      throw new Error('invalid relatory')
+
+    const hbs = new HandlebarsCompilerService(this.payload.template)
+    const html = await hbs.compile({
+      name: this.payload.contact.name,
+      games: this.payload.games!.gamesRelatory,
+      totalPrice: this.payload.games!.totalPrice
+    })
+
+    await transporter.sendMail({
+      from: 'TGL BETS 🍀 <tgl@suport.com>',
+      to: this.payload.contact.email,
+      subject: 'Monthly reports',
+      html
+    })
+  }
+}
