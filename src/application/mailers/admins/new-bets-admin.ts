f60a0f7a555a78333f1249ed8dc53dbb4b938c76
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-21 09:32:41 -0300

    feat: add new-bets-admin

---
+++ src/application/mailers/admins/new-bets-admin.ts
@@ -0,0 +1,35 @@
+import { Transporter } from "nodemailer";
+import SMTPTransport from "nodemailer/lib/smtp-transport";
+import BaseMailer from "../../../infra/mailer/base-mailer";
+import IPayloadMailer from "../interfaces/IPayloadMailer";
+import HandlebarsCompilerService from "../../../infra/handlebars/handlebars";
+
+export default class NewBetAdminMailer extends BaseMailer {
+  constructor(authConfig: SMTPTransport.Options, private payload: IPayloadMailer) {    
+    super(authConfig)
+  }
+
+  async prepare(transporter: Transporter): Promise<void> {
+
+    if(!this.payload.contact || !this.payload.contact.name || !this.payload.contact.email)
+      throw new Error('invalid contact')
+
+    if(!this.payload.bets || this.payload.bets.arrayBets.length === 0 || !this.payload.bets.totalPrice)
+      throw new Error('invalid bets')
+
+    const hbs = new HandlebarsCompilerService(this.payload.template)
+    const html = await hbs.compile({
+      name: this.payload.contact.name,
+      username: this.payload.username,
+      bets: this.payload.bets!.arrayBets,
+      totalPrice: this.payload.bets?.totalPrice
+    })
+
+    await transporter.sendMail({
+      from: 'TGL BETS 🍀 <tgl@suport.com>',
+      to: this.payload.contact.email,
+      subject: 'Bets placed!',
+      html
+    })
+  }
+}
