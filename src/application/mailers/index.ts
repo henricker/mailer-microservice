4463dfeae54c7f18bf4ff1d491a19b4446b39d91
Henrique Vieira <henriquevieira@alu.ufc.br>
2021-09-21 09:35:31 -0300

    refactor: add new-bets-admin property and rename new-bet-user to new-bets-user

--- src/application/mailers/index.ts
+++ src/application/mailers/index.ts
@@ -1,17 +1,19 @@
 import WelcomeUserMailer from "./users/welcome-user"
 import ForgotPasswordUserMailer from "./users/forgot-password-user"
-import NewBetUserMailer from "./users/new-bet-user"
+import NewBetUserMailer from "./users/new-bets-user"
 import RememberUserToBetMailer from "./users/remember-user-to-bet"
 import DailyReportsAdminMailer from "./admins/daily-reports"
 import WeeklyReportsAdminMailer from "./admins/weekly-reports"
 import MonthlyReportsAdminMailer from "./admins/monthly-reports"
+import NewBetAdminMailer from './admins/new-bets-admin'
 
 export default {
 	'welcome-user': WelcomeUserMailer,
 	'forgot-password-user': ForgotPasswordUserMailer,
-	'new-bet-user': NewBetUserMailer,
+	'new-bets-user': NewBetUserMailer,
 	'remember-user-to-bet': RememberUserToBetMailer,
 	'daily-reports-admin': DailyReportsAdminMailer,
 	'weekly-reports-admin': WeeklyReportsAdminMailer,
 	'monthly-reports-admin': MonthlyReportsAdminMailer,
+	'new-bets-admin': NewBetAdminMailer
 }
